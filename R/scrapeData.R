c2105c1a58684da64b7ea5c3ac6eea82ac9f0700
wagnertimo <wagnertimo@gmx.de>
2017-06-03 13:17:06 +0200

    Added 30min and 15min data.

--- R/scrapeData.R
+++ R/scrapeData.R
@@ -62,6 +62,17 @@ getIntradayContinuousEPEXSPOT <- function(startDate, endDate, product = "60") {
   # Therefore it is good to start with the loop at the last date, then the day before the last date can be also on the table
   dates_array = seq(sdate, edate, by="days")
 
+
+  # url = paste("https://www.epexspot.com/en/market-data/intradaycontinuous/intraday-table/", dates_array[length(dates_array)], "/DE", sep="")
+  #
+  # payload = list();
+  #
+  # postResponse <- POST(url, body = payload, encode = "form")
+  #
+  # parsedHtml <- htmlParse(content(postResponse, "text", encoding = "UTF-8"))
+  # return(parseICEPEXSPOT(parsedHtml))
+
+
   r = data.frame()
   # Init progress bar // CAUTION --> the length of auctionIds can be longer than needed (retrieves all auctionIds but stops at the input end date)
   if(getOption("logging")) pb <- txtProgressBar(min = 0, max = length(dates_array) - 1, style = 3)
@@ -77,7 +88,7 @@ getIntradayContinuousEPEXSPOT <- function(startDate, endDate, product = "60") {
     postResponse <- POST(url, body = payload, encode = "form")
 
     parsedHtml <- htmlParse(content(postResponse, "text", encoding = "UTF-8"))
-    r <- rbind(r, parseICEPEXSPOT(parsedHtml))
+    r <- rbind(r, parseICEPEXSPOT(parsedHtml, product))
 
     # update progress bar
     if(getOption("logging")) setTxtProgressBar(pb, length(dates_array) - i + 1)
@@ -99,67 +110,93 @@ getIntradayContinuousEPEXSPOT <- function(startDate, endDate, product = "60") {
 #
 # parses target website for data of first date and second date on the site
 # CAUTION what if not two dates are displayed!!! --> BUT it seems that always two are displayed
-parseICEPEXSPOT <- function(htmlDoc) {
+parseICEPEXSPOT <- function(htmlDoc, product) {
   library(logging)
   library(XML)
 
   if(getOption("logging")) loginfo(paste("parseICEPEXSPOT - Parsing Continuous Intraday EPEX website with 2 dates"))
 
   date_list <- as.Date(xpathSApply(htmlDoc, "id('content')/div/table/tbody/tr[1]/th[contains(@class, 'date')]/text()", saveXML), "%d/%m/%Y")
+  # Get the Base and Peak index price for both dates
+  index_price_list <- xpathSApply(htmlDoc, "id('content')/div/table/tbody/tr/th[contains(@class, 'date')]/text()", saveXML)
+  base_1 <- sapply(strsplit(gsub("\n", "", gsub(" ", "", index_price_list[3:6], fixed = TRUE)), ":"), function(x) as.numeric(x[2]))[1]
+  peak_1 <- sapply(strsplit(gsub("\n", "", gsub(" ", "", index_price_list[3:6], fixed = TRUE)), ":"), function(x) as.numeric(x[2]))[2]
+  base_2 <- sapply(strsplit(gsub("\n", "", gsub(" ", "", index_price_list[3:6], fixed = TRUE)), ":"), function(x) as.numeric(x[2]))[3]
+  peak_2 <- sapply(strsplit(gsub("\n", "", gsub(" ", "", index_price_list[3:6], fixed = TRUE)), ":"), function(x) as.numeric(x[2]))[4]
 
   # Time: Hour (First 00 - 01) --> But only 00 needed
-  # id('content')/x:div/x:table/x:tbody/x:tr[4]/x:td[3]
   # ---> contains all time slots: id('content')/div/table/tbody/tr/td[contains(@class, 'title')]
   times_list <- xpathSApply(htmlDoc, "id('content')/div/table/tbody/tr/td[contains(@class, 'title')]/text()", saveXML)
   # Clean the strings --> remove newline and whitespcaes
   times_list <- gsub("\n", "", gsub(" ", "", times_list, fixed = TRUE))
-  # ---> every 7th entry is hour data --> e.g. 1. 8. 15. 22. ......
+
   # get every hour: 00-01 01-02 ...and also reduce to 00 01 ... and append by ":00" for nice format 00:00 01:00 ...
-  hour_list <- sapply(strsplit(times_list[seq(1, length(times_list)-1, 7)], "-"), function(x) paste(x[1], ":00", sep=""))
+  # for other product shift start by 1 (30min) 2 (15min)
+  # Default value is hour start and freq
+  if(product == "60") {
+    # Pattern for 1h: start: 1 freq: every 7th entry is hour data --> e.g. 1. 8. 15. 22. ......
+    start_freq <- seq(1, length(times_list)-1, 7)
+    # Set the xpath expression to get the right td elements
+    xpath <- "id('content')/div/table/tbody/tr/td[contains(@class, 'toggle_30min_info_closed')]/../td/text()"
+  }
+  else if(product == "30") {
+    # Pattern for 30min:+3 then +4 ---> 2 5 9 12 16 ...
+    start_freq <- cseq(2, length(times_list)-1, c(3,4))
+    # Set the xpath expression to get the right td elements
+    xpath <- "id('content')/div/table/tbody/tr/td[contains(@class, 'toggle_15min_info_closed')]/../td/text()"
+  }
+  else if(product == "15") {
+    # Pattern for 15min: +1 then +2 then +1 then +3 ... --> 3 4 6 7 10 11 13 14 17 ...
+    start_freq <- cseq(3, length(times_list)-1, c(1,2,1,3))
+    # Set the xpath expression to get the right td elements
+    xpath <- "id('content')/div/table/tbody/tr[contains(@id, 'intra_15')]/td/text()"
+  }
+  else{
+    print("WRONG PRODUCT CODE - CHOOSE 60, 30 or 15 as character input!")
+  }
 
-  # Last priceof first date
+  # Get the times
+  times_list <- sapply(strsplit(times_list[start_freq], "-"), function(x) x[1])
+  # Only for hourly data: Append by ":00" for nice format 00:00 01:00 ...
+  times_list <- if(product == "60") paste(times_list, ":00", sep="") else paste(times_list, "", sep="")
+
   # Gets every td elements of table (also times etc)
-  tds_list <- xpathSApply(htmlDoc, "id('content')/div/table/tbody/tr/td[contains(@class, 'toggle_30min_info_closed')]/../td/text()", saveXML)
-  # every 17th entry starting at the 4th e.g. 4. 21. ...
-  last_price_1 <- as.numeric(tds_list[seq(4, length(tds_list)-1, 17)])
-  low_1 <- as.numeric(tds_list[seq(2, length(tds_list)-1, 17)])
-  high_1 <- as.numeric(tds_list[seq(3, length(tds_list)-1, 17)])
-  weighted_avg_1 <- as.numeric(tds_list[seq(5, length(tds_list)-1, 17)])
-  idx_1 <- as.numeric(tds_list[seq(6, length(tds_list)-1, 17)])
-  id3_1 <- as.numeric(tds_list[seq(7, length(tds_list)-1, 17)])
-  buy_vol_1 <- gsub(",", "", tds_list[seq(8, length(tds_list)-1, 17)])
-  sell_vol_1 <- gsub(",", "", tds_list[seq(9, length(tds_list), 17)])
-  # CAUTION: WHAT IF NO SECOND PRICE ?????
-  # Last price of second date
-  # every 17th entry starting at the 12th e.g. 12. 29. ...
-  last_price_2 <- as.numeric(tds_list[seq(12, length(tds_list)-1, 17)])
-  low_2 <- as.numeric(tds_list[seq(10, length(tds_list)-1, 17)])
-  high_2 <- as.numeric(tds_list[seq(11, length(tds_list)-1, 17)])
-  weighted_avg_2 <- as.numeric(tds_list[seq(13, length(tds_list)-1, 17)])
-  idx_2 <- as.numeric(tds_list[seq(14, length(tds_list)-1, 17)])
-  id3_2 <- as.numeric(tds_list[seq(15, length(tds_list)-1, 17)])
-  buy_vol_2 <- gsub(",", "", tds_list[seq(16, length(tds_list)-1, 17)])
-  sell_vol_2 <- gsub(",", "", tds_list[seq(17, length(tds_list), 17)])
+  tds_list <- xpathSApply(htmlDoc, xpath, saveXML)
 
-  # CAUTION: WHAT IF NO SECOND PRICE ?????
-  # Combine Dates and hours to a DateTime array
-  d <- as.POSIXct(c(paste(date_list[1], hour_list), paste(date_list[2], hour_list)), tz = "Europe/Berlin")
+  # initialize data.frames with DateTime for the two dates on the site
+  df1 <- data.frame(DateTime = as.POSIXct(c(paste(date_list[1], times_list)), tz = "Europe/Berlin"))
+  df2 <- data.frame(DateTime = as.POSIXct(c(paste(date_list[2], times_list)), tz = "Europe/Berlin"))
+  # add columns/variables to the initial data.frames
+  for(i in 2:17) {
+    # after 10 the next date starts
+    if(i < 10) {
+      # 2nd is Low 3rd High .... buy and sell vol have comma seperated value fot thousands
+      if(i == 8 | i == 9) column <- as.numeric(gsub(",", "", tds_list[seq(i, length(tds_list), 17)])) else column <- as.numeric(tds_list[seq(i, length(tds_list)-1, 17)])
+      df1 <- cbind(df1, i = column)
+    }
+    else {
+      # buy and sell vol have comma seperated value fot thousands
+      if(i == 16 | i == 17) column <- as.numeric(gsub(",", "", tds_list[seq(i, length(tds_list), 17)])) else column <- as.numeric(tds_list[seq(i, length(tds_list)-1, 17)])
+      df2 <- cbind(df2, i = column)
+    }
+  }
 
-  df <- data.frame(DateTime = d,
-                   Last = c(last_price_1, last_price_2),
-                   Low = c(low_1, low_2),
-                   High = c(high_1, high_2),
-                   Weighted_Avg = c(weighted_avg_1, weighted_avg_2),
-                   Idx = c(idx_1, idx_2),
-                   ID3 = c(id3_1, id3_2),
-                   Buy_Vol = c(buy_vol_1, buy_vol_2),
-                   Sell_Vol = c(sell_vol_1, sell_vol_2)
-                   )
+  df1 <- cbind(df1, Index_Base = base_1, Index_Peak = peak_1)
+  df2 <- cbind(df2, Index_Base = base_2, Index_Peak = peak_2)
+  r <- rbind(df1, df2)
+  colnames(r) <- c("DateTime","Low","High","Last","Weighted_Avg","Idx","ID3","Buy_Vol","Sell_Vol","Index_Base","Index_Peak")
 
-
-  return(df)
+  return(r)
+  #return(index_price_list)
 }
 
+# Helper function for @seealso parseICEPEXSPOT
+# Allows complex patterns for sequence method
+cseq <- function(from, to, by){
+  times <- (to-from) %/% sum(by)
+  x <- cumsum(c(from, rep(by, times+1)))
+  x[x<=to]
+}
 
 
 #'
--- testScript.R
+++ testScript.R
@@ -4,18 +4,10 @@
 
 setLogging(FALSE)
 
-b <- getIntradayContinuousEPEXSPOT("2017-05-20", "2017-05-26", "60")
-b
-plot(h, type="l")
-head(h)
+c <- getIntradayContinuousEPEXSPOT("2017-05-23", "2017-05-25", "15")
+class(c)
+c
 
-as.numeric(gsub(",", "", b[1,9]))
+rm(b,h)
 
-
-# DateTime Last
-# 1 2017-05-20 00:00:00 26.9
-# 2 2017-05-20 01:00:00 16.5
-# 3 2017-05-20 02:00:00 22.1
-# 4 2017-05-20 03:00:00 15.0
-# 5 2017-05-20 04:00:00 22.9
-# 6 2017-05-20 05:00:00 24.0
+names(c)
